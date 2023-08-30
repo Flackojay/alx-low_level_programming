5b23b65bb76db2f011c551608fe19ed01e2da81b
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-08-30 08:52:53 -0700

    corrected

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,11 @@
  */
 int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	if (n < 0)
-		n = -n;
+	if (last < 0)
+		last *= -1;
 
-	last = n % 10;
 	_putchar('0' + last);
 	return (last);
-	_putchar('\n');
 }
