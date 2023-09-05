2b6a1192bf680af389e7e90c1070d8b1eb0927ab
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-09-05 03:44:26 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 if succesful
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
