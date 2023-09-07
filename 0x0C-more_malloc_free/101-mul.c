91ea099e02c45fa29da712e489b99e0cf89a5c50
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-09-07 16:28:34 -0700

    okayish

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,147 +1,68 @@
 #include <stdlib.h>
-#include "main.h"
-
+#include <stdio.h>
 /**
- * _strlen - Calculates the length of a string.
- * @str: The input string.
- *
- * Return: The length of the string.
- */
-int _strlen(char *str)
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
+  */
+int _isdigit(char *argv)
 {
-	int len = 0;
+	int i;
 
-	while (str[len])
-		len++;
-
-	return (len);
-}
-
-/**
- * is_digit - Checks if a string is composed of digits.
- * @str: The input string.
- *
- * Return: 1 if composed of digits, 0 otherwise.
- */
-int is_digit(char *str)
-{
-	if (!str || *str == '\0')
-		return (0);
-
-	while (*str)
+	i = 0;
+	while (argv[i])
 	{
-		if (*str < '0' || *str > '9')
-			return (0);
-		str++;
-	}
-
-	return (1);
-}
-
-/**
- * multiply - Multiplies two positive numbers.
- * @num1: First number as a string.
- * @num2: Second number as a string.
- *
- * Return: The result of the multiplication as a string.
- */
-char *multiply(char *num1, char *num2)
-{
-	int len1 = _strlen(num1);
-	int len2 = _strlen(num2);
-	int len = len1 + len2;
-	int *result = calloc(len, sizeof(int));
-
-	if (!result)
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
-	}
-
-	for (int i = len1 - 1; i >= 0; i--)
-	{
-		int carry = 0;
-		int n1 = num1[i] - '0';
-
-		for (int j = len2 - 1; j >= 0; j--)
-		{
-			int n2 = num2[j] - '0';
-			int product = n1 * n2 + result[i + j + 1] + carry;
-
-			result[i + j + 1] = product % 10;
-			carry = product / 10;
-		}
-	}
-
-	char *res = malloc((len + 1) * sizeof(char));
-	if (!res)
-	{
-		free(result);
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
-	}
-
-	int i = 0;
-	while (i < len && result[i] == 0)
-		i++;
-
-	if (i == len)
-		res[0] = '0';
-	else
-	{
-		int j = 0;
-		while (i < len)
-		{
-			res[j] = result[i] + '0';
+		if (argv[i] >= '0' && argv[i] <= '9')
 			i++;
-			j++;
-		}
-		res[j] = '\0';
+		else
+			return (1);
 	}
-
-	free(result);
-	return (res);
-}
-
-/**
- * main - Entry point
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: 0 on success, 98 on incorrect arguments or non-digits.
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
-	}
-
-	char *result = multiply(argv[1], argv[2]);
-	int len = _strlen(result);
-
-	for (int i = 0; i < len; i++)
-		_putchar(result[i]);
-
-	_putchar('\n');
-	free(result);
-
 	return (0);
 }
+/**
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+int _atoi(char *s)
+{
+	int i, result;
 
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+/**
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	malloc();
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	return (0);
+}
