82a40bb8c5d0544d363f0713880dd4ad21a39592
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-10-18 02:03:37 -0700

    dont swap horses in a crossing stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to integer a
+ * @b: pointer to integer b
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+0x05. C - Pointers, arrays and strings
+C
+ By: Julien Barbier
+ Weight: 1
+ Project over - took place from Aug 21, 2023 6:00 AM to Aug 22, 2023 6:00 AM
+ An auto review will be launched at the deadline
