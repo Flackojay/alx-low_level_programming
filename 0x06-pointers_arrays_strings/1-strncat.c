088b0bbb5ede85c61e72710e626ab62ab78c0a18
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-10-22 17:28:38 -0700

    strncat crosschecked

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,10 +16,11 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	while (j < n && src[j] != ' ')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i + j] = src[j];
 		j++;
 	}
+	dest[i + j] = '\0';
 	return (dest);
 }
