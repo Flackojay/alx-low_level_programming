837f83515cde6d4e06d69646b393a3c78e2f37dc
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-10-23 08:44:22 -0700

    hopefully

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,19 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	int len = 0;
 
-	while (i < n && src[i] != '\0' )
+	while (dest[len] != '\0')
+		len++;
+
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	if (src[n] == '\0')
+		dest[i] = '\0';
+	else
+		dest[len] = '\0';
 	return (dest);
 }
