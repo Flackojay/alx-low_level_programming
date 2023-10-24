3dbd4b3d825e9470d632af5e2398be4ad0a83d97
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-10-24 09:33:09 -0700

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * is_separator - checks if a character is a word separator
+ * @c: character to check
+ *
+ * Return: 1 if character is a separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+    char separators[] = " \t\n,;.!?\"(){}";
+    int i;
+
+    for (i = 0; separators[i] != '\0'; i++)
+    {
+        if (c == separators[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: input string
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+    int capitalize = 1; /* Variable to track if the next character should be capitalized */
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (is_separator(str[i]))
+        {
+            capitalize = 1;
+        }
+        else if (capitalize && str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - ('a' - 'A'); /* Convert to uppercase */
+            capitalize = 0;
+        }
+        else
+        {
+            capitalize = 0;
+        }
+    }
+
+    return str;
+}
