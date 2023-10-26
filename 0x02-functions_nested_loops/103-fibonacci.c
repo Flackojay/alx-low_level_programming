e3d7768441b62b48724c6f5e07dd8b7e193e26db
Flackojay <olubusoye37@gmail.com>
2023-10-26 16:07:21 -0700

    even liber abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - calculates the sum of even-valued terms in the
+ * fibonacci sequence where values do not exceed 4000000.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned int first = 1;
+	unsigned int second = 2;
+	unsigned int next;
+	int sum = 0;
+
+	while (second <= 4000000)
+	{
+		if (second % 2 == 0)
+		{
+			sum += second;
+		}
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	printf("%u\n", sum);
+}
