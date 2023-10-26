560d87182f50b13328f7cb7dfee13cca95a5c5e1
Flackojay <olubusoye37@gmail.com>
2023-10-25 19:41:03 -0700

    wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int is_palindrome_checker(char *s, int start, int end);
 int str_length(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
