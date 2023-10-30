5b85e80fc447efcaa8eff25f34c9624ea13cfa46
Flackojay <olubusoye37@gmail.com>
2023-10-30 14:36:10 -0700

    encoding to rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13: encodes a string using rot13
+ * @str: string to encode
+ *
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = rot[j];
+			}
+		}
+	}
+	return (str);
+}
+
