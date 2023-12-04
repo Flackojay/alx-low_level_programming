a886187f981a0c7a46d71842678ce3a12c6f93da
Pelumi Olubusoye <olubusoye37@gmail.com>
2023-12-03 19:11:49 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,37 @@
 #include <stdlib.h>
 
 /**
- * dog_t - typedef for dog structure
+ * custom_strdup - duplicates string
+ * @src: source string
+ *
+ * Return: dest string or NULL
+ */
+char *custom_strdup(const char *src)
+{
+	int length, i;
+	char *dest;
+
+	if (src == NULL)
+		return (NULL);
+
+	length = 0;
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	dest = malloc(length * sizeof(char));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: pointer to dog name
  * @age: age of dog
@@ -12,11 +42,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog = malloc(sizeof(dog_t));
+	dog_t *newDog;
+
+	newDog = malloc(sizeof(dog_t));
+
 	if (newDog == NULL)
+	{
 		return (NULL);
-	newDog->name = name;
+	}
+	newDog->name = (name != NULL) ? custom_strdup(name) : NULL;
+	newDog->owner = (owner != NULL) ? custom_strdup(owner) : NULL;
+
+	if (newDog->name == NULL && name != NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	if (newDog->owner == NULL && owner != NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
 	newDog->age = age;
-	newDog->owner = owner;
+
 	return (newDog);
 }
