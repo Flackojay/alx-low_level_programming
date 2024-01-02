a9ccbea5ad49e01a00f66aadb3e07a5cad1a7aaa
Pelumi Olubusoye <olubusoye37@gmail.com>
2024-01-02 13:01:53 -0800

    printing other half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string. Followed by a new line
+ * @str: pointer to string
+ */
+void puts_half(char *str)
+{
+	int length = 0, n, i;
+
+	while (str[length] != '\0')
+		length++;
+	n = (length - 1) / 2;
+
+	if (length % 2 == 0)
+	{
+		for (i = (length / 2); i < length; i++)
+			_putchar(str[i]);
+	}
+	else
+		for (i = n; i < length; i++)
+			_putchar(str[i]);
+	_putchar('\n');
+}
