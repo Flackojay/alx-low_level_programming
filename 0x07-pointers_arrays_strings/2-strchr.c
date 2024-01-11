11e463eb6f01274e7e0c7fc9a10aa8e92dea6544
Pelumi Olubusoye <olubusoye37@gmail.com>
2024-01-11 14:03:34 -0800

    memcpy and strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * strchr - locates a character in a string
+ * @s: string to check character
+ * @c: character to locate
+ *
+ * Return: a pointer to the first occurrence of the character in the
+ * string or NULL if character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
