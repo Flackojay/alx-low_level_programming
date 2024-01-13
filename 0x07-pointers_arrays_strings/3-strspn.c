c2d549c8682f8ff923ee226e8fb1a54cec2c26a2
Pelumi Olubusoye <olubusoye37@gmail.com>
2024-01-12 20:36:42 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, count = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
+			{
 				count += 1;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
 		}
 	}
 	return (count);
