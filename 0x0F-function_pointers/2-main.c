c1126d7d59c146a79f3c6cd87b929b62babd119c
Pelumi Olubusoye <olubusoye37@gmail.com>
2024-08-02 17:29:28 -0700

    works properly now

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -2,12 +2,23 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - check if a umber is equal to 98
+ * is_98 - check if a number is equal to 98
  * @elem: the integer to check
  *
  * Return: 0 if false, something else otherwise.
  */
 int is_98(int elem)
+{
+	return (98 == elem);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
 {
 	return (elem > 0);
 }
